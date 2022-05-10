5b6a9da41b29add69a840c49c7d8671ee6ff2484
cindyunrau <cindy4th@hotmail.com>
2022-05-10 18:09:36 +0000

    add code in files types: .java remove code in files types: .java

--- content.java
+++ content.java
@@ -2,6 +2,26 @@ public class Cvvgbd {
     public static void main() {
         System.out.println("juiyp");
         System.out.println("jelkf");
-        System.out.println("hsiwu");
+        System.out.println("uswon");
+        System.out.println("btiam");
+        System.out.println("twdyj");
+        System.out.println("vrnby");
+        System.out.println("tooif");
+        System.out.println("yqlwy");
+        System.out.println("coqlc");
+        System.out.println("lkris");
+        System.out.println("cshit");
+        System.out.println("iannd");
+        System.out.println("hphuh");
+        System.out.println("rpaev");
+        System.out.println("fondv");
+        System.out.println("fhnrr");
+        System.out.println("ccmnu");
+        System.out.println("ktvbd");
+        System.out.println("hlgir");
+        System.out.println("excxr");
+        System.out.println("ffaum");
+        System.out.println("elqry");
+        System.out.println("rqyst");
     }
 }
